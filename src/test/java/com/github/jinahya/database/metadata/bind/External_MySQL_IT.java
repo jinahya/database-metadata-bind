492659a198d49995d022982b9fa8c1e0b25b14c1
Jin Kwon <onacit@gmail.com>
2025-12-04 10:18:57 +0900

    wip

---
+++ src/test/java/com/github/jinahya/database/metadata/bind/External_MySQL_IT.java
@@ -0,0 +1,80 @@
+package com.github.jinahya.database.metadata.bind;
+
+/*-
+ * #%L
+ * database-metadata-bind
+ * %%
+ * Copyright (C) 2011 - 2019 Jinahya, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.DriverManager;
+
+/**
+ * A test class for binding a remote database.
+ *
+ * @author Jin Kwon &lt;onacit at gmail.com&gt;
+ */
+@Slf4j
+class External_MySQL_IT {
+
+    private static final String PROPERTY_NAME_URL = "url";
+
+    private static final String PROPERTY_NAME_USER = "user";
+
+    private static final String PROPERTY_NAME_PASSWORD = "password";
+
+    private static final String PROPERTY_NAME_CATALOG = "catalog";
+
+    @Disabled
+    @EnabledIfSystemProperty(named = PROPERTY_NAME_URL, matches = ".+")
+    @EnabledIfSystemProperty(named = PROPERTY_NAME_USER, matches = ".+")
+    @EnabledIfSystemProperty(named = PROPERTY_NAME_PASSWORD, matches = ".+")
+    @Test
+    void columns() throws Exception {
+        final var clazz = Class.forName("com.mysql.cj.jdbc.Driver");
+        log.debug("loaded: {}", clazz);
+        final String url = System.getProperty("url");
+        final String user = System.getProperty("user");
+        final String password = System.getProperty("password");
+        log.info("connecting to {}", url);
+        try (var connection = DriverManager.getConnection(url, user, password)) {
+            log.info("connected: {}", connection);
+            final var context = Context.newInstance(connection);
+            final var mapper = new ObjectMapper();
+            final var factory = mapper.getFactory();
+            try (final var stream = new FileOutputStream("columns.json")) {
+                final var generator = factory.createGenerator(stream);
+                generator.writeStartArray();
+                context.acceptColumns(null, null, "%", "%", c -> {
+                    try {
+                        generator.writeObject(c);
+                    } catch (final IOException ioe) {
+                        throw new RuntimeException(ioe);
+                    }
+                });
+                generator.writeEndArray();
+            }
+        }
+    }
+}
